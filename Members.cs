e738229625e52812000800f43c58758e83c9555e
Fannabykiki <namplhe153292@fpt.edu.vn>
2022-10-03 03:00:16 +0700

    Initial commit

---
+++ Members.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rookies.core
+{
+    public class Members
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Gender { get; set; }
+        private DateTime DateOfBirth { get; set; }
+        private string PhoneNumber { get; set; }
+        private string BirthPlace { get; set; }
+
+        private int age { get; set; }
+        private bool Graduated { get; set; }
+        
+    }
+    
+}
