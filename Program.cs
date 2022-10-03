b7c1655b09e82d606c71334e4d0f5dfb3384e03b
Fannabykiki <namplhe153292@fpt.edu.vn>
2022-10-03 11:52:18 +0700

    Update

--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@ namespace Rookies.core // Note: actual namespace depends on the project name.
         {
 
             int n;
-            System.Console.WriteLine("Case 0:Show all");
-            System.Console.WriteLine("Case 1:A list of members who is Male");
-            System.Console.WriteLine("Case 2:The oldest one based on “Age”");
-            System.Console.WriteLine("Case 3:A new list that contains Full Name only");
+            System.Console.WriteLine("Case 0 :Show all");
+            System.Console.WriteLine("Case 1 :A list of members who is Male");
+            System.Console.WriteLine("Case 2 :The oldest one based on “Age”");
+            System.Console.WriteLine("Case 3 :A new list that contains Full Name only");
             System.Console.WriteLine("Case 4 :List of members who has birth year is 2000");
             System.Console.WriteLine("Case 5 :List of members who has birth year greater than 2000");
             System.Console.WriteLine("Case 6 :List of members who has birth year less than 2000");
@@ -26,16 +26,19 @@ namespace Rookies.core // Note: actual namespace depends on the project name.
             mb.Add(new Member { FirstName = "Dao", LastName = "Trang", Gender = "FeMale", DateOfBirth = new DateTime(2003, 07, 13), PhoneNumber = "0396373132", BirthPlace = "SG", Age = 29, IsGraduated = true });
             mb.Add(new Member { FirstName = "Vu", LastName = "Kim", Gender = "Male", DateOfBirth = new DateTime(2003, 11, 30), PhoneNumber = "0396373132", BirthPlace = "Ha Noi", Age = 29, IsGraduated = true });
             mb.Add(new Member { FirstName = "Duy", LastName = "Anh", Gender = "Male", DateOfBirth = new DateTime(2000, 11, 30), PhoneNumber = "0396373132", BirthPlace = "Ha Noi", Age = 21, IsGraduated = true });
+           do{
             try
             {
                 n = Convert.ToInt32(Console.ReadLine());
                 switch (n)
                 {
-                    case 0:
+
+                    case 0:                 
                         foreach (var item in mb)
                         {
                             Console.WriteLine(item);
                         }
+
                         break;
                     case 1:
                         var result1 = (from m in mb
@@ -46,6 +49,7 @@ namespace Rookies.core // Note: actual namespace depends on the project name.
                         {
                             Console.WriteLine(item);
                         }
+
                         break;
                     case 2:
                         var result2 = mb.OrderByDescending(s => s.Age).ThenBy(s => s.DateOfBirth.Month).Distinct().FirstOrDefault().ToString();
@@ -54,61 +58,77 @@ namespace Rookies.core // Note: actual namespace depends on the project name.
                         {
                             Console.Write(item);
                         }
+
                         break;
                     case 3:
                         var result3 = mb.Select(s => s.FirstName + " " + s.LastName).Aggregate((a, b) => a + ", " + b);
+
                         foreach (var item in result3)
                         {
                             Console.Write(item);
                         }
+
                         break;
                     case 4:
                         var result4 = (from m in mb
                                        where m.DateOfBirth.Year.Equals(2000)
                                        select m).ToList();
+
                         foreach (var item in result4)
                         {
                             Console.WriteLine(item);
                         }
+
                         break;
 
                     case 5:
                         var result5 = (from m in mb
                                        where ((uint)m.DateOfBirth.Year > 2000)
                                        select m).ToList();
+
                         foreach (var item in result5)
                         {
                             Console.WriteLine(item);
                         }
+
                         break;
                     case 6:
                         var result6 = (from m in mb
                                        where ((uint)m.DateOfBirth.Year < 2000)
                                        select m).ToList();
+
                         foreach (var item in result6)
                         {
                             Console.WriteLine(item);
                         }
+
                         break;
                     case 7:
                         var result7 = (from m in mb
                                        where m.BirthPlace.Contains("Ha Noi")
                                        select m).Distinct().FirstOrDefault().ToString();
+
                         foreach (var item in result7)
                         {
                             Console.Write(item);
+                            
                         }
+
                         break;
                     default:
-                        System.Console.WriteLine("Enter integer number from 0 - 7 pls");
+
+                        System.Console.WriteLine("Enter integer number from 0 - 7 ");
+
                         return;
                 }
             }
             catch (Exception)
             {
-                System.Console.WriteLine("Enter integer number pls");
+                System.Console.WriteLine("Enter integer number please");
                 return;
             }
+           }while(n >=0 && n <= 7);
+           Console.ReadKey();
         }
     }
 }
